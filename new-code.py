d5806ebd052e411bc0d72aa4f3f09445ea8dc7c7
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2026-05-11 09:09:16 -0500

    Update new-code.py

--- new-code.py
+++ new-code.py
@@ -3,6 +3,6 @@ Traceback (most recent call last):
     self.edit_panel.load_issue(issue)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^
   File "c:\Users\P09816\OneDrive - NGC\Desktop\Coding\JIRA_manager\sprintmate.py", line 1164, in load_issue
-    print(f"ASSIGNEE FIELD: {assignee}\nMembers Sample: {self._members[:2] if self._members else "EMPTY"}")
+    print(f"ASSIGNEE FIELD: {assignee}\nMembers Sample: {self._members[:2] if self._members else 'EMPTY'}")
                              ^^^^^^^^
 UnboundLocalError: cannot access local variable 'assignee' where it is not associated with a value
