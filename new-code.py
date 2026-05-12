3dc19cea9fbb4d3e296da32653b3b2e6deb1fa18
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2026-05-12 09:00:59 -0500

    Update new-code.py

--- new-code.py
+++ new-code.py
@@ -1,6 +1,1 @@
-try:
-    issue_types = self._client.get_issue_types(key)
-except Exception:
-    issue_types = [{"name": self.edit_panel.issuetype_combo.itemText(i),
-                    "id": self.edit_panel.issuetype_combo.itemData(i)}
-                   for i in range(self.edit_panel.issuetype_combo.count())]
+self.type_combo.addItem(it.get("name", "?"), it.get("id"))
