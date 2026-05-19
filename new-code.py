75dd5750179e1eefe5e41df465566d89bec8e5dc
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2026-05-19 10:30:48 -0500

    Update new-code.py

--- new-code.py
+++ new-code.py
@@ -1,1 +1,8 @@
-text
+Traceback (most recent call last):
+  File "c:\Users\P09816\OneDrive - NGC\Desktop\Coding\JIRA_manager\sprintmate.py", line 3186, in _on_story_selected
+    self.edit_panel.load_issue(issue)
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^
+  File "c:\Users\P09816\OneDrive - NGC\Desktop\Coding\JIRA_manager\sprintmate.py", line 1726, in load_issue
+    self.undo_btn.setEnabled(False)
+    ^^^^^^^^^^^^^
+AttributeError: 'StoryEditPanel' object has no attribute 'undo_btn'
